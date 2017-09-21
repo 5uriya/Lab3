f6dd805927534651b503d50021dddabb0613a1f8
Balaji Ramkumar <balaji512ramkumar@gmail.com>
2017-09-21 22:15:04 +0200

    commit data

---
+++ Lab3/data/data_info.R
@@ -0,0 +1,20 @@
+#' Wiki_Graph
+#'
+#' Wiki_Graph is a data frame of nodes and weights
+#' V1= initial node, V2= neighbouring node, W= weight
+#'
+#' @docType data
+#'
+#' @usage data(wiki_graph)
+#'
+#' @format An object
+#'
+#' @keywords datasets
+#'
+#' @references \href{http://www.r-tutor.com/r-introduction/data-frame}{R tutorial}
+#'
+#' @source From the lab manual
+#' 
+#' @examples dijkstra(graph = wiki_graph, init_node = 1)
+
+"wiki_graph"
